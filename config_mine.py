fb787773e80ac81597fc4f0a3c948de491f65436
Andrew Kling <klinga88@gmail.com>
2018-06-25 13:05:19 -0400

    Delete config_mine.py

--- config_mine.py
+++
@@ -1,7 +0,0 @@
-# Twitter API Keys
-consumer_key = "AsCMUP0uk5zfJhgCanMd75skd"
-consumer_secret = "vw8sBpR80ZwJRauMOq76eyvZLLVEGdFHzG82M88LF4b09FYsl5"
-access_token = "1007988157835628544-6OCWiY4ynyYIlcWdvk9FJCZzvzRQB0"
-access_token_secret = "zYiocHhYxpvFPEYb062h86fptSd2DtY06CAB2C9sBkdua"
-weather_api_key = "9dcaf0d7730dfbc9c8ed4035cdffc92e"
-alphavantage_api_key = "LEU6X8JULIFJN4UM"
