bb932b0af8ff114d2fbbcc99e35d1241c3b07cbf
klinga <klinga88@gmail.com>
2018-06-26 11:15:47 -0400

    Finalized creation of dataframe for maximum profit

---
+++ Stock_Time_Machine.py
@@ -0,0 +1,6 @@
+1. open individual file
+2. iterate over days and update min close price, max close price, max and max difference
+3. for each new day that has a lower min start calculating the max difference again
+4. if max difference is ever larger than the previous, save that instead
+5. Now you have this for one stock, do it for all the stocks!!!!
+
